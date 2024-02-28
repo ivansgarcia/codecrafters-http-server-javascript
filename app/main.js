980363eba48a477f57f35faf0ee31b051cb39360
ivansalinas <ivansalinasgarcia@gmail.com>
2024-02-28 13:13:28 +0100

    pass 4nd stage

--- app/main.js
+++ app/main.js
@@ -7,7 +7,7 @@ const server = net.createServer((socket) => {
         const dataArray = data.toString().split(CRLF);
         console.log('DATA: ', dataArray);
         const requestType = stringData[0].split(' ')[0];
-        const args = stringData[0].split(' ')[1];
+        const args = dataArray[0].split(' ')[1];
         if (args.split('/')[1] == 'echo') {
             socket.write('HTTP/1.1 200 OK' + CRLF + 'Content-Type: text/plain' + CRLF + 'Content-Length: ' + args.split('/')[2].length + CRLF + CRLF + args.split('/')[2]);
         }
