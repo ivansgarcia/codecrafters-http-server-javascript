f1da2f6247c3e0007e767da5ebe4c2d13e229986
ivansalinas <ivansalinasgarcia@gmail.com>
2024-02-28 13:02:17 +0100

    pass 3nd stage

--- app/main.js
+++ app/main.js
@@ -5,7 +5,15 @@ const CRLF = '\r\n';
 const server = net.createServer((socket) => {
     socket.on('data', (data) => {
         console.log('data', data.toString());
-        socket.write('HTTP/1.1 200 OK' + CRLF + CRLF);
+        const stringData = data.toString().split(CRLF);
+        const requestType = stringData[1].split(' ')[1];
+        const path = stringData[1].split(' ')[2];
+        if (path == '/') {
+            socket.write('HTTP/1.1 200 OK' + CRLF + CRLF);
+        }
+        else {
+            socket.write('HTTP/1.1 404 NOT FOUND' + CRLF + CRLF);
+        }
     })
     socket.on('close', () => {
         socket.end();
