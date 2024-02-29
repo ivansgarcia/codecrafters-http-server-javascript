92ee032ef33ca837c5945866686d50dc8817a251
ivansalinas <ivansalinasgarcia@gmail.com>
2024-02-29 09:57:18 +0100

    pass 7nd stage

--- app/main.js
+++ app/main.js
@@ -7,21 +7,32 @@ const CRLF = '\r\n';
 const server = net.createServer((socket) => {
     socket.on('data', (data) => {
         const dataArray = data.toString().split(CRLF);
+        console.log('ARGS: ', process.argv);
         console.log('DATA: ', dataArray);
         const requestType = dataArray[0].split(' ')[0];
         const args = dataArray[0].split(' ')[1];
-        // console.log(args);
-        const userAgent = dataArray[2].split(' ')[1]
+        const userAgent = dataArray[2].split(' ')[1];
+
         if (args.startsWith('/echo/')) {
-            socket.write('HTTP/1.1 200 OK' + CRLF + 'Content-Type: text/plain' + CRLF + 'Content-Length: ' + args.slice('/echo/'.length, args.length).length + CRLF + CRLF + args.slice('/echo/'.length, args.length));
+            const echoValue = args.slice('/echo/');
+            socket.write('HTTP/1.1 200 OK' + CRLF + 'Content-Type: text/plain' + CRLF + 'Content-Length: ' + echoValue.length + CRLF + CRLF + echoValue);
+            return socket.end();
         }
         if (args == '/user-agent') {
-            socket.write('HTTP/1.1 200 OK' + CRLF + 'Content-Type: text/plain' + CRLF + 'Content-Length: ' + userAgent.length + CRLF + CRLF + userAgent)
+            socket.write('HTTP/1.1 200 OK' + CRLF + 'Content-Type: text/plain' + CRLF + 'Content-Length: ' + userAgent.length + CRLF + CRLF + userAgent);
+            return socket.end();
         }
         if (args.startsWith('/files/')) {
             const filename = args.slice('/files/'.length, args.length);
-            // const filePath = path.resolve(args)
-            console.log(process.argv[3]);
+            const filePath = path.resolve(process.argv[3], filename);
+            if (fs.existsSync(filePath)) {
+                const fileContent = fs.readFileSync(filePath);
+                socket.write('HTTP/1.1 200 OK' + CRLF + 'Content-Type: octet-stream' + CRLF + 'Content-Length: ' + fileContent.length + CRLF + CRLF + fileContent);
+                return socket.end();
+            } else {
+                socket.write('HTTP/1.1 404 NOT FOUND' + CRLF + CRLF);
+                return socket.end();
+            }
         }
         else if (args == '/') {
             socket.write('HTTP/1.1 200 OK' + CRLF + CRLF);
@@ -37,3 +48,4 @@ const server = net.createServer((socket) => {
 });
 
 server.listen(4221, 'localhost');
+console.log('PROCESS.ARGV: ', process.argv);
